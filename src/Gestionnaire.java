6f4d9cd1331ac1dad1656d0a870d5489d3951787
tgaudier <tgaudier@enssat.fr>
2019-06-03 17:02:27 +0200

    charger/sauvegarder tout

--- src/Gestionnaire.java
+++ src/Gestionnaire.java
@@ -9,6 +9,7 @@ public class Gestionnaire {
     private ArrayList<Client> clients = new ArrayList<>();
     private ArrayList<Contrat> contrats = new ArrayList<>();
 
+    /** Vehicules **/
 
     public ArrayList<Vehicule> getVehicules() {
         return vehicules;
@@ -25,6 +26,44 @@ public class Gestionnaire {
     public void ajouterVehicule (Vehicule v) {
         vehicules.add(v);
     }
+
+    /** Clients **/
+
+    public ArrayList<Client> getClients() {
+        return clients;
+    }
+
+    public Client getClient (int id) {
+        return clients.get(id);
+    }
+
+    public int getNbClients () {
+        return clients.size();
+    }
+
+    public void ajouterClient (Client c) {
+        clients.add(c);
+    }
+
+    /** Contrats **/
+
+    public ArrayList<Contrat> getContrats() {
+        return contrats;
+    }
+
+    public Contrat getContrat (int id) {
+        return contrats.get(id);
+    }
+
+    public int getNbContrats () {
+        return contrats.size();
+    }
+
+    public void ajouterContrat (Contrat c) {
+        contrats.add(c);
+    }
+
+    /** XML **/
 
     public void chargerVehicules () {
         XMLDecoder decoder;
@@ -56,4 +95,65 @@ public class Gestionnaire {
         encoder.close();
     }
 
+
+    public void chargerClients () {
+        XMLDecoder decoder;
+        try {
+            decoder = new XMLDecoder(new FileInputStream("clients.xml"));
+        } catch (FileNotFoundException e) {
+            decoder = null;
+            e.printStackTrace();
+        }
+
+        assert decoder != null;
+        clients = (ArrayList<Client>)decoder.readObject();
+
+        decoder.close();
+    }
+
+    public void sauvegarderClients () {
+        XMLEncoder encoder;
+        try {
+            encoder = new XMLEncoder(new FileOutputStream("clients.xml"));
+        } catch (FileNotFoundException e) {
+            encoder = null;
+            e.printStackTrace();
+        }
+
+        assert encoder != null;
+        encoder.writeObject(clients);
+
+        encoder.close();
+    }
+
+
+    public void chargerContrats () {
+        XMLDecoder decoder;
+        try {
+            decoder = new XMLDecoder(new FileInputStream("contrats.xml"));
+        } catch (FileNotFoundException e) {
+            decoder = null;
+            e.printStackTrace();
+        }
+
+        assert decoder != null;
+        contrats = (ArrayList<Contrat>)decoder.readObject();
+
+        decoder.close();
+    }
+
+    public void sauvegarderContrats () {
+        XMLEncoder encoder;
+        try {
+            encoder = new XMLEncoder(new FileOutputStream("contrats.xml"));
+        } catch (FileNotFoundException e) {
+            encoder = null;
+            e.printStackTrace();
+        }
+
+        assert encoder != null;
+        encoder.writeObject(contrats);
+
+        encoder.close();
+    }
 }
