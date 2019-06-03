d5c728612d0cc82d8c0f4af5e335db94656c9103
tgaudier <tgaudier@enssat.fr>
2019-06-03 16:24:16 +0200

    pouêt

--- src/GestionnaireVehicules.java
+++ src/GestionnaireVehicules.java
@@ -1,6 +1,9 @@
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.*;
 import java.util.ArrayList;
 
-public class GestionnaireVehicules {
+public class GestionnaireVehicules implements Serializable {
 
     private ArrayList<Vehicule> vehicules = new ArrayList<>();
 
@@ -16,6 +19,18 @@ public class GestionnaireVehicules {
         vehicules.add(v);
     }
 
+    public void chargerVehicules () {
+        XMLDecoder decoder;
+        try {
+           decoder = new XMLDecoder(new FileInputStream("vehicules.json"));
+        } catch (FileNotFoundException e) {
+            decoder = null;
+            e.printStackTrace();
+        }
 
+        ajouterVehicule((Vehicule)decoder.readObject());
+
+        decoder.close();
+    }
 
 }
